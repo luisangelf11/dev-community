72491e85ddcf73bcf549dc4b4e0ae742b0111481
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2024-11-10 00:08:49 -0400

    design chat modify

--- client/src/home/components/Chat.tsx
+++ client/src/home/components/Chat.tsx
@@ -11,6 +11,7 @@ import { useStore } from "../../context/useStore";
 import Message from "./Message";
 import { io, Socket } from "socket.io-client";
 import { apiUrl } from "../../config/api";
+import "../../App.css";
 
 type Props = {
   roomId: number;
@@ -71,7 +72,7 @@ export default function Chat({ roomId }: Props) {
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     handleSendMessage();
-    getRoomData()
+    getRoomData();
   };
 
   return (
@@ -79,15 +80,21 @@ export default function Chat({ roomId }: Props) {
       <div className="w-full flex justify-center items-center p-2">
         <h3 className="uppercase font-semibold">{room?.room}</h3>
       </div>
-      <div className=" w-full h-[400px] bg-neutral-700 overflow-auto p-2">
-        {room?.messages.map((el) => (
-          <Message
-            key={el.id}
-            message={el.message}
-            create_at={el.createAt}
-            userId={el.userId}
-          />
-        ))}
+      <div className=" w-full h-[400px] bg-neutral-700 overflow-auto p-2 new-scroll">
+        {room?.messages.length ? (
+          room?.messages.map((el) => (
+            <Message
+              key={el.id}
+              message={el.message}
+              create_at={el.createAt}
+              userId={el.userId}
+            />
+          ))
+        ) : (
+          <p className="text-center uppercase text-sm font-semibold">
+            Chat is empty
+          </p>
+        )}
       </div>
       <form
         onSubmit={handleSubmit}
@@ -103,7 +110,14 @@ export default function Chat({ roomId }: Props) {
           }
         />
         <button className="text-xs uppercase bg-blue-700 p-2 transition-all hover:bg-blue-400 rounded-sm">
-          Send
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-4"
+          >
+            <path d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+          </svg>
         </button>
       </form>
     </article>
